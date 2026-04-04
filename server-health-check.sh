778204b8a6197d5a8b5cb54c36077801fe509737
DEV-man <253447196+shailesh-Ops@users.noreply.github.com>
2026-04-04 11:40:27 +0000

    this file are use server status show

---
+++ server-health-check.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# "NOW I AM GOING SERVER AUTOMATION SCRIPT WITH BASH LANGUAGE...."
+
+echo "THIS IS IP WITH ITS CONECTIONS POINT...SHOW"
+ifconfig
+
+echo "______________________________________________________"
+
+echo "THIS IS CPU USE CASE SHOW USING SERVER ON LOCALLY....SHOW"
+lscpu
+
+echo "______________________________________________________"
+
+echo "THIS IS MEMORY USE CASE OF USING SERVER ON LOCALLY....SHOW"
+free -h
+
+echo "______________________________________________________"
+
+echo "THIS IS DISK USE CASE OF USING SRRVER ON LOCALLY....SHOW"
+df -Th
+
+echo "______________________________________________"
+
+echo "THIS SERVER HEALTH CHECK ARE COMPLITED¹Å"
