521f8b4ae58a75baa8c9599ef27e4ca0dbde94ea
devbhojaiya <253447196+shailesh-Ops@users.noreply.github.com>
2026-05-04 06:47:45 +0000

    this is new system update for virtual memory update

--- system.sh
+++ system.sh
@@ -1,2 +1,12 @@
 echo ".................................................."
-top  
+top 
+
+echo "----------------------------------------------------"
+echo "this is current time of system up time"
+uptime
+
+echo "-----------------------------------------"
+echo "this show virtual memory use case..... using cm--vmstat..."
+vmstat
+
+
